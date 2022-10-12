ac42dba898c1cd33cecc44a81b2490631fbd8843
RizkyMilan <mhmrizky33@gmail.com>
2022-10-12 16:14:27 +0700

    add Login

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcrypt")
+
 const userController = {
     postRegister: async (req, res) => {
         try {
@@ -11,8 +13,46 @@ const userController = {
 
     },
     loginUser: async (req, res) => {
-        try {
-            
+        try { 
+            const { usernameOrEmail, password } = req.body
+
+            const findUserByUsernameOrEmail = await User.findOne({
+                where: {
+                    [Op.or]: {
+                        username: usernameOrEmail,
+                        email: usernameOrEmail,
+                    },
+                },
+            })
+
+            if (!findUserByUsernameOrEmail) {
+                return res.status(400).json({
+                    message: "User Not Found"
+                })
+            }
+
+            const passwordValid = bcrypt.compareSync(
+                password,
+                findUserByUsernameOrEmail.password
+            )
+
+            if (!passwordValid) {
+                return res.status(400).json({
+                    message: "Wrong password"
+                })
+            }
+
+            delete findUserByUsernameOrEmail.dataValues.password
+
+            const token = signToken({
+                id: findUserByUsernameOrEmail.id,
+            })
+
+            return res.status(201).json({
+                message: "Login user",
+                data: findUserByUsernameOrEmail,
+                token,
+            })
         } catch (error) {
             console.log(error)
             return res.status(500).json({
